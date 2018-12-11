06082843f2299a67a067be9ac24aba92eb324f0c
Marcin Frasz <noahy21@gmail.com>
2018-12-11 07:38:06 +0100

    Dodaj pliki projektów.

---
+++ Programowanie_w_srodowisku_Windows/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Programowanie_w_srodowisku_Windows
+{
+    partial class Form1_serial_port_communication
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1_com_ports = new System.Windows.Forms.ComboBox();
+            this.button_send_reset = new System.Windows.Forms.Button();
+            this.richTextBox_text_to_send = new System.Windows.Forms.RichTextBox();
+            this.button_send_text = new System.Windows.Forms.Button();
+            this.groupbox_mode = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button_open_com = new System.Windows.Forms.Button();
+            this.button_com_close = new System.Windows.Forms.Button();
+            this.radioButton_DL = new System.Windows.Forms.RadioButton();
+            this.radioButton_PHY = new System.Windows.Forms.RadioButton();
+            this.radioButton_BPSK = new System.Windows.Forms.RadioButton();
+            this.radioButton_QPSK = new System.Windows.Forms.RadioButton();
+            this.radioButton_8PSK = new System.Windows.Forms.RadioButton();
+            this.checkBox_FEC = new System.Windows.Forms.CheckBox();
+            this.groupbox_mode.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1_com_ports
+            // 
+            this.comboBox1_com_ports.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1_com_ports.FormattingEnabled = true;
+            this.comboBox1_com_ports.Location = new System.Drawing.Point(32, 13);
+            this.comboBox1_com_ports.Name = "comboBox1_com_ports";
+            this.comboBox1_com_ports.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1_com_ports.TabIndex = 0;
+            this.comboBox1_com_ports.SelectedIndexChanged += new System.EventHandler(this.comboBox1_com_ports_SelectedIndexChanged);
+            // 
+            // button_send_reset
+            // 
+            this.button_send_reset.Location = new System.Drawing.Point(170, 11);
+            this.button_send_reset.Name = "button_send_reset";
+            this.button_send_reset.Size = new System.Drawing.Size(112, 23);
+            this.button_send_reset.TabIndex = 1;
+            this.button_send_reset.Text = "SEND RESET";
+            this.button_send_reset.UseVisualStyleBackColor = true;
+            this.button_send_reset.Click += new System.EventHandler(this.button_send_reset_Click);
+            // 
+            // richTextBox_text_to_send
+            // 
+            this.richTextBox_text_to_send.Location = new System.Drawing.Point(337, 13);
+            this.richTextBox_text_to_send.Name = "richTextBox_text_to_send";
+            this.richTextBox_text_to_send.Size = new System.Drawing.Size(309, 121);
+            this.richTextBox_text_to_send.TabIndex = 2;
+            this.richTextBox_text_to_send.Text = "";
+            // 
+            // button_send_text
+            // 
+            this.button_send_text.Location = new System.Drawing.Point(352, 140);
+            this.button_send_text.Name = "button_send_text";
+            this.button_send_text.Size = new System.Drawing.Size(129, 23);
+            this.button_send_text.TabIndex = 3;
+            this.button_send_text.Text = "SEND TEXT";
+            this.button_send_text.UseVisualStyleBackColor = true;
+            // 
+            // groupbox_mode
+            // 
+            this.groupbox_mode.Controls.Add(this.radioButton_PHY);
+            this.groupbox_mode.Controls.Add(this.radioButton_DL);
+            this.groupbox_mode.Location = new System.Drawing.Point(32, 81);
+            this.groupbox_mode.Name = "groupbox_mode";
+            this.groupbox_mode.Size = new System.Drawing.Size(121, 100);
+            this.groupbox_mode.TabIndex = 4;
+            this.groupbox_mode.TabStop = false;
+            this.groupbox_mode.Text = "MODE";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox_FEC);
+            this.groupBox2.Controls.Add(this.radioButton_8PSK);
+            this.groupBox2.Controls.Add(this.radioButton_QPSK);
+            this.groupBox2.Controls.Add(this.radioButton_BPSK);
+            this.groupBox2.Location = new System.Drawing.Point(179, 88);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(117, 105);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // button_open_com
+            // 
+            this.button_open_com.Location = new System.Drawing.Point(32, 41);
+            this.button_open_com.Name = "button_open_com";
+            this.button_open_com.Size = new System.Drawing.Size(75, 23);
+            this.button_open_com.TabIndex = 6;
+            this.button_open_com.Text = "COM OPEN";
+            this.button_open_com.UseVisualStyleBackColor = true;
+            this.button_open_com.Click += new System.EventHandler(this.button_open_com_Click);
+            // 
+            // button_com_close
+            // 
+            this.button_com_close.Location = new System.Drawing.Point(123, 41);
+            this.button_com_close.Name = "button_com_close";
+            this.button_com_close.Size = new System.Drawing.Size(85, 23);
+            this.button_com_close.TabIndex = 7;
+            this.button_com_close.Text = "COM CLOSE";
+            this.button_com_close.UseVisualStyleBackColor = true;
+            this.button_com_close.Click += new System.EventHandler(this.button_com_close_Click);
+            // 
+            // radioButton_DL
+            // 
+            this.radioButton_DL.AutoSize = true;
+            this.radioButton_DL.Location = new System.Drawing.Point(0, 20);
+            this.radioButton_DL.Name = "radioButton_DL";
+            this.radioButton_DL.Size = new System.Drawing.Size(39, 17);
+            this.radioButton_DL.TabIndex = 0;
+            this.radioButton_DL.TabStop = true;
+            this.radioButton_DL.Text = "DL";
+            this.radioButton_DL.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_PHY
+            // 
+            this.radioButton_PHY.AutoSize = true;
+            this.radioButton_PHY.Location = new System.Drawing.Point(0, 44);
+            this.radioButton_PHY.Name = "radioButton_PHY";
+            this.radioButton_PHY.Size = new System.Drawing.Size(47, 17);
+            this.radioButton_PHY.TabIndex = 1;
+            this.radioButton_PHY.TabStop = true;
+            this.radioButton_PHY.Text = "PHY";
+            this.radioButton_PHY.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_BPSK
+            // 
+            this.radioButton_BPSK.AutoSize = true;
+            this.radioButton_BPSK.Location = new System.Drawing.Point(7, 13);
+            this.radioButton_BPSK.Name = "radioButton_BPSK";
+            this.radioButton_BPSK.Size = new System.Drawing.Size(53, 17);
+            this.radioButton_BPSK.TabIndex = 0;
+            this.radioButton_BPSK.TabStop = true;
+            this.radioButton_BPSK.Text = "BPSK";
+            this.radioButton_BPSK.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_QPSK
+            // 
+            this.radioButton_QPSK.AutoSize = true;
+            this.radioButton_QPSK.Location = new System.Drawing.Point(7, 37);
+            this.radioButton_QPSK.Name = "radioButton_QPSK";
+            this.radioButton_QPSK.Size = new System.Drawing.Size(54, 17);
+            this.radioButton_QPSK.TabIndex = 1;
+            this.radioButton_QPSK.TabStop = true;
+            this.radioButton_QPSK.Text = "QPSK";
+            this.radioButton_QPSK.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_8PSK
+            // 
+            this.radioButton_8PSK.AutoSize = true;
+            this.radioButton_8PSK.Location = new System.Drawing.Point(7, 57);
+            this.radioButton_8PSK.Name = "radioButton_8PSK";
+            this.radioButton_8PSK.Size = new System.Drawing.Size(52, 17);
+            this.radioButton_8PSK.TabIndex = 2;
+            this.radioButton_8PSK.TabStop = true;
+            this.radioButton_8PSK.Text = "8PSK";
+            this.radioButton_8PSK.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_FEC
+            // 
+            this.checkBox_FEC.AutoSize = true;
+            this.checkBox_FEC.Location = new System.Drawing.Point(7, 81);
+            this.checkBox_FEC.Name = "checkBox_FEC";
+            this.checkBox_FEC.Size = new System.Drawing.Size(46, 17);
+            this.checkBox_FEC.TabIndex = 3;
+            this.checkBox_FEC.Text = "FEC";
+            this.checkBox_FEC.UseVisualStyleBackColor = true;
+            // 
+            // Form1_serial_port_communication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 280);
+            this.Controls.Add(this.button_com_close);
+            this.Controls.Add(this.button_open_com);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupbox_mode);
+            this.Controls.Add(this.button_send_text);
+            this.Controls.Add(this.richTextBox_text_to_send);
+            this.Controls.Add(this.button_send_reset);
+            this.Controls.Add(this.comboBox1_com_ports);
+            this.Name = "Form1_serial_port_communication";
+            this.Text = "Serial_port_communication";
+            this.groupbox_mode.ResumeLayout(false);
+            this.groupbox_mode.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1_com_ports;
+        private System.Windows.Forms.Button button_send_reset;
+        private System.Windows.Forms.RichTextBox richTextBox_text_to_send;
+        private System.Windows.Forms.Button button_send_text;
+        private System.Windows.Forms.GroupBox groupbox_mode;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton_DL;
+        private System.Windows.Forms.Button button_open_com;
+        private System.Windows.Forms.Button button_com_close;
+        private System.Windows.Forms.RadioButton radioButton_PHY;
+        private System.Windows.Forms.CheckBox checkBox_FEC;
+        private System.Windows.Forms.RadioButton radioButton_8PSK;
+        private System.Windows.Forms.RadioButton radioButton_QPSK;
+        private System.Windows.Forms.RadioButton radioButton_BPSK;
+    }
+}
+
---
+++ Programowanie_w_srodowisku_Windows/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Timers;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading; 
+
+// nie ciągnąć w dół przy wysyłaniu ACK
+namespace Programowanie_w_srodowisku_Windows
+{
+    public partial class Form1_serial_port_communication : Form
+    {
+        static SerialPort serialPort = new SerialPort();
+        string[] ports = SerialPort.GetPortNames();
+        Queue data_queue = new Queue();
+        Queue byte_Queue = new Queue();
+        private Queue<byte> RxData = new Queue<byte>();
+        byte[] byteBuffer;
+        List<byte> frame = new List<byte>();
+        List<byte> frame_gotowa = new List<byte>();
+        private static System.Timers.Timer aTimer;
+        Thread thread;
+        private STATE BUFFER_STATE;
+
+        private enum STATE
+        {
+            LOOKING_FOR_BEGIN,
+            LOOKING_FOR_LEN,
+            LOOKING_FOR_COMAND_CODE,
+            LOOKING_FOR_DATA_COLLECTING,
+            LOOKING_FOR_CHECK_BYTE_ONE,
+            LOOKING_FOR_CHECK_BYTE_TWO,
+
+        }
+
+        public Form1_serial_port_communication()
+        {
+            InitializeComponent();
+            foreach (string port in ports)
+            {
+                comboBox1_com_ports.Items.Add(port);
+            }
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(responseHandler);
+        }
+
+        private static void SetTimer(int time)
+        {
+
+            aTimer = new System.Timers.Timer(time);
+            aTimer.Enabled = true;
+        }
+
+        private void create_frame(byte[] tab, byte V)
+        {
+            byte len = Convert.ToByte(tab.Length);
+            byte[] frame = new byte[5 + tab.Length];
+            int index = 0;
+            frame[index++] = 0x02;
+        }
+
+        public void ramka()
+        {
+            while (true)
+            {
+                switch (BUFFER_STATE)
+                {
+                    case STATE.LOOKING_FOR_BEGIN:
+                        if (byte_Queue.Count != 0)
+                        {
+                            byte received = Convert.ToByte(byte_Queue.Dequeue());
+                            if (received == 0x02 || received == 0x03)
+                            {
+                                // frame_gotowa.Add(received);
+                                BUFFER_STATE = STATE.LOOKING_FOR_LEN;
+                                Console.WriteLine("mam poczatek");
+                                Thread.Sleep(1000);
+                            }
+                        }
+                        break;
+
+                    case STATE.LOOKING_FOR_LEN:
+                        if (data_queue.Count != 0)
+                        {
+                            byte c = Convert.ToByte(byte_Queue.Dequeue());
+                            frame_gotowa.Add(c);
+                            Console.WriteLine("MAM DLUGOSC");
+                            BUFFER_STATE = STATE.LOOKING_FOR_COMAND_CODE;
+                        }
+                        break;
+
+                    case STATE.LOOKING_FOR_COMAND_CODE:
+                        if (byte_Queue.Count != 0)
+                        {
+
+                            byte c = Convert.ToByte(byte_Queue.Dequeue());
+                            frame_gotowa.Add(c);
+                            Console.WriteLine("mam cc");
+                            BUFFER_STATE = STATE.LOOKING_FOR_DATA_COLLECTING;
+
+
+                        }
+                        break;
+
+                    case STATE.LOOKING_FOR_DATA_COLLECTING:
+                        if (byte_Queue.Count != 0)
+                        {
+                            byte c = Convert.ToByte(byte_Queue.Dequeue());
+                            if (c == 0x00)
+                            {
+                                Console.WriteLine("mam date");
+                                BUFFER_STATE = STATE.LOOKING_FOR_CHECK_BYTE_ONE;
+                            }
+                            else
+                            {
+                                frame_gotowa.Add(c);
+                            }
+
+
+                        }
+                        break;
+                    case STATE.LOOKING_FOR_CHECK_BYTE_ONE:
+                        if (byte_Queue.Count != 0)
+                        {
+                            byte c = Convert.ToByte(byte_Queue.Dequeue());
+
+                            byte suma = 0;
+
+                            foreach (byte indeks in frame_gotowa)
+                            {
+                                suma += indeks;
+                            }
+                            Console.WriteLine(suma.ToString());
+                            if (c == suma)
+                            {
+                                BUFFER_STATE = STATE.LOOKING_FOR_CHECK_BYTE_TWO;
+                                Console.WriteLine("Suma sie zgadza");
+                            }
+
+
+
+
+                        }
+                        break;
+
+                    case STATE.LOOKING_FOR_CHECK_BYTE_TWO:
+                        if (byte_Queue.Count != 0)
+                        {
+                            byte c = Convert.ToByte(byte_Queue.Dequeue());
+                            if (c == 0x00)
+                            {
+                                Console.WriteLine("mam koniec");
+                                
+                                byte[] sendArray = { 0x06 };
+                                serialPort.Write(sendArray, 0, sendArray.Length);
+                                BUFFER_STATE = STATE.LOOKING_FOR_BEGIN;
+                            }
+
+                        }
+                        break;
+
+                }
+            }
+        }
+
+
+        private void responseHandler(object sender, SerialDataReceivedEventArgs args)
+        {
+            try
+            {
+
+                int intBuffer;
+                intBuffer = serialPort.BytesToRead;
+                 byteBuffer = new byte[intBuffer];
+                serialPort.Read(byteBuffer, 0, intBuffer);
+
+                string res = ByteArrayToString(byteBuffer);
+
+                foreach (byte x in byteBuffer)
+                {
+                    byte_Queue.Enqueue(x);
+                }
+                thread = new Thread(ramka);
+                thread.Start();
+            }
+            catch (Exception ex) { }
+        }
+
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int intBuffer;
+            intBuffer = serialPort.BytesToRead;
+            byteBuffer = new byte[intBuffer];
+            serialPort.Read(byteBuffer, 0, intBuffer);
+            string res = ByteArrayToString(byteBuffer);
+            Console.WriteLine("Odbiór: " + res);
+        }
+
+        private string ByteArrayToString(byte[] byteBuffer)
+        {
+            return BitConverter.ToString(byteBuffer);
+        }
+
+
+        private void comboBox1_com_ports_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_open_com_Click(object sender, EventArgs e)
+        {
+            serialPort = new SerialPort(comboBox1_com_ports.Text);
+            serialPort.DataBits = 8;
+            serialPort.Parity = Parity.None;
+            serialPort.StopBits = StopBits.One;
+            serialPort.BaudRate = 57600;
+            
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(Port_DataReceived);
+            serialPort.Open();
+            Debug.Print("Połączony z " + comboBox1_com_ports.Text);
+        }
+
+        private void button_com_close_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort.Close();
+                Console.WriteLine(comboBox1_com_ports.SelectedItem.ToString() + " CLOSE");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception close");
+            }
+        }
+
+        private void button_send_reset_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+                serialPort.RtsEnable = true;
+                 byte[] sendArray = { 0x02, 0x00, 0x3C, 0x3C, 0x00 };
+               //byte[] sendArray = { 0x3F, 0x06, 0x02, 0x01, 0x3D, 0x00, 0x3E, 0x00 };
+                serialPort.Write(sendArray, 0, sendArray.Length);
+                serialPort.RtsEnable = false;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Catched " + exc);
+                MessageBox.Show("Nie wysłało bład transmisji ");
+            }
+        }
+    }
+
+
+}
